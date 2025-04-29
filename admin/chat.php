24c230eda9629777d81bdbb1593b4d2e727718f9
Usama Bashir <usamab@mudsoft.tech>
2025-04-29 11:59:17 +0500

    first commit

--- admin/chat.php
+++ admin/chat.php
@@ -3,27 +3,7 @@
 
 
 <head>
-  <meta charset="utf-8">
-  <title>NewVilla - Real Estate HTML Template</title>
-  <meta name="description" content="Morden Bootstrap HTML5 Template">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
-    
-   <!-- ======= All CSS Plugins here ======== -->
-  <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css">
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&amp;family=Nunito:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
-
-  
-  <!-- Plugin css -->
-  <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css">
-  <link rel="stylesheet" href="assets/css/plugins/glightbox.min.css">
-
-  <!-- Custom Style CSS -->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/dashboard.css">
-  <link rel="stylesheet" href="assets/css/chat.css">
-  <link rel="stylesheet" href="assets/css/dark.css">
+<?php include 'includes/css-links.php' ?>
 
   <script>
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer footer__section">
+  <div class="dashboard__footer--inner text-center">
+    <p class="copyright__content mb-0">Copyright © 2025 Powered By <span>AGR Soft Sahiwal</span>. All Rights Reserved.</p>
+  </div>
+</footer>
