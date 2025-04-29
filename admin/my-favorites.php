63e8cb2ab09e5618ec2f509db006c6ad338bff3d
Usama Bashir <usamab@mudsoft.tech>
2025-04-29 12:00:40 +0500

    first commit

--- admin/my-favorites.php
+++ admin/my-favorites.php
@@ -905,29 +905,9 @@
         </div>
       </div> 
 
-    <!-- Scroll top bar -->
-    <button id="scroll__top"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round"  stroke-width="48" d="M112 244l144-144 144 144M256 120v292"/></svg></button>
-    
-   <!-- All Script JS Plugins here  -->
-   <script src="assets/js/vendor/popper.js" defer="defer"></script>
-   <script src="assets/js/vendor/bootstrap.min.js" defer="defer"></script>
-   <script src="assets/js/plugins/swiper-bundle.min.js"></script>
-   <script src="assets/js/plugins/glightbox.min.js"></script>
+    <!-- ======= All JS here ======== -->
+    <?php include 'includes/js-links.php' ?>
 
-
-  <!-- Customscript js -->
-  <script src="assets/js/script.js"></script>
-
-   <!-- Dark to light js -->
-   <script>
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (localStorage.getItem("theme-color") === "dark" || (!("theme-color" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-      document.getElementById("light__to--dark")?.classList.add("dark--version");
-    } 
-    if (localStorage.getItem("theme-color") === "light") {
-      document.getElementById("light__to--dark")?.classList.remove("dark--version");
-    } 
-  </script>
 
   
 </body>
